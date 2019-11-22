177dc02d502a231959c0c31f75b6c34439b42716
Tyler Serrato <tserrato21351@columbushs.com>
2019-11-21 21:53:14 -0500

    progress

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -25,8 +25,13 @@ function orderByYear(newArray) {
 console.log(orderByYear(movies));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+function howManyMovies(movieList) {
+    let newList = movieList.filter(movies => movies.director === 'Steven Spielberg');
+    return newList.filter(movies => movies.genre.includes('Drama')).length;
+}
 
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
